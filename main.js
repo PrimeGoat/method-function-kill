2a263942d2505178f07859108e0c33e2d3993d9d
DENIS SAVGIR <denis@DENISs-MBP.home>
2020-04-08 10:26:50 -0400

    Done

--- main.js
+++ main.js
@@ -2,6 +2,40 @@
  * YOUR CODE HERE *
  ******************/
 
+const newPerson = function(first = "Anonymous", last = "Person", years = 0, marriage = false) {
+  return {
+    firstName: first,
+    lastName: last,
+    age: years,
+    married: marriage,
+
+    goingOn: function() {
+      return this.age + 1;
+    },
+
+    ageUp: function() {
+      this.age++;
+    },
+
+    getFullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+
+    marry: function(person) {
+      this.married = true;
+      person.married = true;
+      this.spouseName = person.firstName + ' ' + person.lastName;
+      person.spouseName = this.firstName + ' ' + this.lastName;
+    },
+
+    divorce: function(person) {
+      this.married = false;
+      person.married = false;
+      delete this.spouseName;
+      delete person.spouseName;
+    }
+  };
+}
 
 
 /*********************************
